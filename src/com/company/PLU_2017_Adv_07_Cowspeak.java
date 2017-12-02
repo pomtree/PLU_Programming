ecf27ca5dbce9a8e5dafc3fe9c9192b115df3300
Tom <im-the@hot-shot.com>
2017-12-01 23:27:38 -0800

    [Solved] PLU_2017_Adv_07_Cowspeak

--- src/com/company/PLU_2017_Adv_07_Cowspeak.java
+++ src/com/company/PLU_2017_Adv_07_Cowspeak.java
@@ -24,23 +24,27 @@ public class PLU_2017_Adv_07_Cowspeak {
     public static void main(String args[]) {
         Scanner s = new Scanner(System.in);
         int dc = s.nextInt();
+        String a = s.nextLine();
         String out[] = new String[dc];
-        System.out.println();
+        String in[] = new String[dc];
         for(int i = 0; i < dc; i++) {
-            String line = s.nextLine();
-            line = s.nextLine();
-            String spl[] = line.split(" ");
+            in[i] = s.nextLine();
+        }
+        for(int i = 0; i < dc; i++) {
+
+            String spl[] = in[i].split(" ");
+            out[i] ="";
             for(int word = 0; word < spl.length; word++) {
                 int m = 0, o = 0;
                 while (spl[word].contains("M")) {
                     m++;
                     spl[word] = spl[word].substring(1);
-                    System.out.println(spl[word]);
+                    //System.out.println(spl[word]);
                 }
                 while (spl[word].contains("O")) {
                     o++;
                     spl[word] = spl[word].substring(1);
-                    System.out.println(spl[word]);
+                    //System.out.println(spl[word]);
                 }
                // m/=2;
                // o/=2;
@@ -48,10 +52,11 @@ public class PLU_2017_Adv_07_Cowspeak {
                 //System.out.println((m+ " m o " + o));
                 //System.out.println((m*16+o));
                 out[i] += (char)(m*16+o);
+                //System.out.println(out[i]);
             }
         }
         for(int i = 0; i < dc; i++) {
-            System.out.println(out[i]);
+            System.out.println( out[i]);
         }
     }
 }
