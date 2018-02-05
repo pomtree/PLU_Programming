1d4c705f7718e5ba9f6d7be2d7e3583f3973b9f1
pomtree <im-the@hot-shot.com>
2018-02-04 17:20:19 -0800

    finished 04-adv-2017

---
+++ src/com/company/PLU/PLU_2017_Adv_06_Seed_Bags.java
@@ -0,0 +1,57 @@
+package com.company.PLU;
+
+/*
+6. Seed Bags
+Looking at the space you’ve allotted
+for your garden, you begin to wonder how many bags of seeds you’ll
+need to buy in order to fill it. It’s a little complicated though, as you plant seeds in a unique way. To plant
+your seeds, you simply throw the bag in the air, and the seeds land around yo
+u in a 3x3 square. Given the
+layout of your garden, and in which areas you would like crops to grow, determine how many bags of
+s
+eeds are required. If some seeds
+land where you don’t want them to grow, no worries. You just won’t
+water those. It’s also acce
+ptable for an area to be overlapped with multiple bags of seeds.
+Input
+The first line will contain a single integer
+n
+that indicates the number of data sets that follow.  Each data set
+will start with a single integer
+s
+(1<=
+s
+<=8) denoting the side length o
+f your square garden. The next
+s
+lines will represent the layout of your garden. A ‘.’ will represent a spot that does not need to be seeded,
+while an ‘x’ will represent an area which you want to plant seeds in.
+Output
+For each test case, output the minim
+um number of seed bags you need to plant your garden.
+Sample Input
+2
+5
+xxxxx
+.....
+xxxxx
+.....
+xxxxx
+8
+.x.xx.x.
+x..x..xx
+..x.xx.x
+.x.x...x
+..xx...x
+x.x..x.x
+..x..x..
+.x..x...
+Sample Output
+4
+7
+ */
+public class PLU_2017_Adv_06_Seed_Bags {
+    public static void main(String args[]) {
+
+    }
+}
